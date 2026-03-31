0aaed9c2661e806672175aecd289742108df7db5
Kemu <tokaruunhi@gmail.com>
2026-03-31 18:51:02 +0700

    Update autobountym1.lua

--- script/autobountym1.lua
+++ script/autobountym1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/53f5fb855d2602f1596d4c89a861613e.lua"))()
+loadstring(game:HttpGet("https://vxezestudio.online/api/scripts/script_iankujtocdo/stream/init"))()
